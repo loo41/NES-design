ee735a958e15d684eee6924154d35dec49b6a22a
markcytian <markcytian@tencent.com>
2021-05-15 23:53:46 +0800

    fix(docz): fix docz router path

--- theme/gatsby-theme-docz/components/Menu/Head.tsx
+++ theme/gatsby-theme-docz/components/Menu/Head.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Input, Icon } from '@components';
 import styles from './index.module.scss';
+import doczSet from '@root/doczrc.js';
 const { name } = require('@root/package.json');
 
 const Head: React.FC<{ setFilterText: (text: string) => void }> = ({
@@ -9,7 +10,7 @@ const Head: React.FC<{ setFilterText: (text: string) => void }> = ({
   return (
     <div className={styles.headWrapper}>
       <div className={styles.header}>
-        <div onClick={() => window.open('/', '_self')}>
+        <div onClick={() => window.open(`${doczSet.base}`, '_self')}>
           {name.split('-')[0].toUpperCase()}-{name.split('-')[1]}
         </div>
         <div>
